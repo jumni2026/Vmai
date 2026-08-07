6416746824d0ba6f8adf0a7d8ded79adf480d6de
jumni2026 <jumni2026@gmail.com>
2026-08-07 23:26:23 +0530

    Create MainActivity with basic structure
    Added a minimal skeleton for MainActivity.

---
+++ app/src/main/java/com/vmax/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vmax.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Skeleton Stage - Minimal stub
+    }
+}
