128d3acd4109e24347ae7f599c4c6593ae538759
jumni2026 <jumni2026@gmail.com>
2026-08-08 16:48:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // Kotlin Compose Compiler Plugin
 }
 
 android {
@@ -17,6 +18,7 @@ android {
 
     buildFeatures {
         buildConfig = true
+        compose = true // Enable Jetpack Compose
     }
 
     buildTypes {
@@ -54,4 +56,12 @@ dependencies {
     implementation("androidx.activity:activity:1.10.1")
 
     testImplementation("junit:junit:4.13.2")
+
+    // Jetpack Compose Dependencies
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 }
