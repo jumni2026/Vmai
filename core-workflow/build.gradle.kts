c1b4168d918d7e4a3fa407de2d2b26d4f63d9f3f
jumni2026 <jumni2026@gmail.com>
2026-08-16 22:19:43 +0530

    Update build.gradle.kts

--- core-workflow/build.gradle.kts
+++ core-workflow/build.gradle.kts
@@ -1,12 +1,19 @@
+plugins {
+    kotlin("jvm")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation(project(":common"))
     implementation(project(":core-model"))
     implementation(project(":core-validation"))
     implementation(project(":core-intelligence"))
     implementation(project(":core-action"))
+    // runtime dependency removed - no longer needed
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-
     testImplementation(kotlin("test"))
 }
