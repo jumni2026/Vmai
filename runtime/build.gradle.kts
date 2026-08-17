f8bbfeb69a21f87e373abf6b138031759b7cb791
jumni2026 <jumni2026@gmail.com>
2026-08-17 08:13:06 +0530

    Add dependencies for Coroutines and ML Kit

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
 }
 
 dependencies {
+
     implementation(project(":common"))
     implementation(project(":core-model"))
     implementation(project(":core-validation"))
@@ -33,8 +34,12 @@ dependencies {
     implementation(project(":core-action"))
     implementation(project(":core-workflow"))
 
+    // Android / Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+    // Google ML Kit - Text Recognition
+    implementation("com.google.mlkit:text-recognition:16.0.1")
 
     testImplementation(kotlin("test"))
 }
