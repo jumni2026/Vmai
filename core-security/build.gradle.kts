43f3bb927a142ed5dbd14cb8b46033163584da57
jumni2026 <jumni2026@gmail.com>
2026-08-19 09:16:27 +0530

    Update build.gradle.kts

--- core-security/build.gradle.kts
+++ core-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
@@ -15,4 +15,8 @@ dependencies {
     implementation(project(":core-action"))
 
     testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
