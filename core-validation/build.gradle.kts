1f5f553ea415bda23cb9b6a5e71aca59b3273d04
jumni2026 <jumni2026@gmail.com>
2026-08-19 09:17:38 +0530

    Upgrade JVM toolchain from 17 to 21

--- core-validation/build.gradle.kts
+++ core-validation/build.gradle.kts
@@ -1,13 +1,19 @@
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
     implementation(project(":common"))
     implementation(project(":core-model"))
+
     testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
