772646eb991d7912bf161bf956ca6467d7a6a465
jumni2026 <jumni2026@gmail.com>
2026-08-23 19:24:59 +0530

    Update build.gradle.kts

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -34,11 +34,9 @@ dependencies {
     implementation(project(":core-action"))
     implementation(project(":core-workflow"))
 
-    // Android / Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
-    // Google ML Kit - Text Recognition
     implementation("com.google.mlkit:text-recognition:16.0.1")
 
     testImplementation(kotlin("test"))
