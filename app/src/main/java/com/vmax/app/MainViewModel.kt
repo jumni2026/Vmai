0fa8b63c0d0d66225696674f9ceed2cf1e2338e4
jumni2026 <jumni2026@gmail.com>
2026-08-24 09:24:10 +0530

    Update MainViewModel.kt

--- app/src/main/java/com/vmax/app/MainViewModel.kt
+++ app/src/main/java/com/vmax/app/MainViewModel.kt
@@ -25,7 +25,7 @@ class MainViewModel : ViewModel() {
     private val _classType = MutableStateFlow("")
     val classType: StateFlow<String> = _classType.asStateFlow()
 
-    // Quota अब String है (Enum नहीं)
+    // Quota String (Enum नहीं)
     private val _quota = MutableStateFlow<String?>(null)
     val quota: StateFlow<String?> = _quota.asStateFlow()
 
